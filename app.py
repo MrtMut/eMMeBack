87bac9f87b94030e197280af0d29a1e653021fda
escribroid <sebascraton@gmail.com>
2024-06-27 23:33:48 -0300

    Initial commit

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ with app.app_context():
 
 # programa principal *******************************
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)  # ejecuta el servidor Flask en el puerto 5000
+    app.run(debug=True, port=5005)  # ejecuta el servidor Flask en el puerto 5005
